f97201cb157176c0dc805feb0ef20e59f2e7ffe6
Ischerba32 <i.scherba32@gmail.com>
2022-05-10 17:28:54 +0300

    added toasts to handle auth errors & greeting feature

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -4,14 +4,35 @@ import IAuthForm from '../../interfaces/authForm.interface';
 import { Button, Card, Input } from '../UI';
 import styles from './AuthForm.module.scss';
 import IAuthFormProps from './AuthForm.props';
+import { ToastContainer, toast } from 'react-toastify';
 
 const AuthForm = ({onSubmit, formAction, actionLink, actionTitle}: IAuthFormProps) => {
   const {register, handleSubmit, formState: { errors }, reset, clearErrors} = useForm<IAuthForm>();
 
+  const formSubmit = ({email, password}: IAuthForm) => {
+    toast.promise(
+      onSubmit({email, password}),
+      {
+        pending: {
+          render(){
+            return "Please wait...";
+          },
+          icon: false,
+        },
+
+        error: {
+          render({data}){
+            // When the promise reject, data will contains the error
+            return `${data.message}`;
+          }
+        }
+      }
+    );};
+
   return (
     <div className={styles.loginForm}>
       <Card color="blue" className={styles.loginCard}>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(formSubmit)}>
           <Input
             {...register('email', {required: { value: true, message: 'Enter the email' }})}
             type="email"
@@ -44,6 +65,17 @@ const AuthForm = ({onSubmit, formAction, actionLink, actionTitle}: IAuthFormProp
         </form>
         {/* {successMsg && <Htag tag="h2">{successMsg}</Htag>} */}
       </Card>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/globals.scss';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
